0c345457109d269b578c424a7dc0a36ecf6aca77
Unknown <basarozcan@gmail.com>
2020-04-29 16:19:10 +0300

    adding galery page

---
+++ src/pages/Gallery.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <div>
+    <div v-for="edge in $page.allPhotos.edges" :key="edge.node.title">
+      <h1 v-html="edge.node.title" />
+      <img :src="edge.node.url_sq">
+    </div>
+  </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  allPhotos {
+    edges {
+      node {
+        content
+        title
+        url_sq
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Gallery'
+  }
+}
+</script>
