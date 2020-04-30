1a7ac51cd6dcd85b09f0de4edaa4c5384ce427e3
Unknown <basarozcan@gmail.com>
2020-04-30 22:44:41 +0300

    normalize.css and style.scss added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+// The Client API can be used here. Learn more: https://gridsome.org/docs/client-api
+
+import '~/assets/styles.scss'
+import '~/assets/normalize.css';
 
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
+
   Vue.component('Layout', DefaultLayout)
 }
+
