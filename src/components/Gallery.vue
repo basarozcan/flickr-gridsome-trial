7667c32bc44f9754b2eec8b4ed801db53c669b0a
Unknown <basarozcan@gmail.com>
2020-04-30 22:53:56 +0300

    components added

---
+++ src/components/Gallery.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="item-wrapper">
+        <g-link :to="item.path">
+          <g-image :alt="item.id" :src="item.url_sq"/>
+        </g-link>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallery',
+  props: ['item'],
+}
+</script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul class="list-group">
+      <li v-for="edge in $static.cats.edges" :key="edge.node.id" class="list-group-item">
+        <g-link :to="edge.node.path">{{ edge.node.title }}</g-link>
+      </li>
+    </ul>
+</template>
+
+<static-query>
+  query Categories{
+    cats: allCategories {
+      edges {
+        node {
+          id
+          title
+          tag
+          path
+        }
+      }
+    }
+  }
+</static-query>
+
+<script>
+export default {
+  name: 'Sidebar',
+}
+</script>
